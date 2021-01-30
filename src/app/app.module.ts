9e3967d0cd9b1605bd38b9bb7131497e4c5e5d5c
dawsonGitHub830 <dawson.on@gmail.com>
2021-01-30 17:47:42 -0500

    in main branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,5 +36,7 @@ export class AppModule {
     // const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
 
     // console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+    //main branch
+    
   }
 }
